4062366a2f617dea9da32a2cabfaae1d5147a12c
Fahad-Anwar <sjdff1@gmail.com>
2018-05-26 07:35:37 +0200

    RTA CHA implemented

--- src/main/java/analysis/exercise2/RTAAlgorithm.java
+++ src/main/java/analysis/exercise2/RTAAlgorithm.java
@@ -1,14 +1,21 @@
 package analysis.exercise2;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.stream.Stream;
-
-import org.junit.Assert;
 
 import analysis.CallGraph;
 import analysis.exercise1.CHAAlgorithm;
+import soot.Body;
 import soot.Scene;
+import soot.SootClass;
 import soot.SootMethod;
+import soot.Unit;
+import soot.jimple.InvokeExpr;
+import soot.jimple.Stmt;
 
 public class RTAAlgorithm extends CHAAlgorithm  {
 
@@ -23,53 +30,67 @@ public class RTAAlgorithm extends CHAAlgorithm  {
         // To get your entry points we prepared getEntryPoints(scene) in the superclass for you
     	Stream<SootMethod> entryPoints = getEntryPoints(scene);
 		entryPoints.forEach(entryPoint -> {
-//			System.out.println(entryPoint.getSignature().toString());
-			if (entryPoint.hasActiveBody() && entryPoint.getSignature().toString()
-					.equals("<target.exercise2.Starter: void main(java.lang.String[])>")) {
-				
-
-/*
-				// things actually instantiated
-		        Assert.assertTrue(callsFromMain.contains(leafMethod));
-		        Assert.assertTrue(callsFromMain.contains(otherLeafMethod));
-		        Assert.assertTrue(callsFromMain.contains(specializationMethod));
-		        Assert.assertTrue(callsFromMain.contains(subclassMethod));
-*/
-				SootMethod method = scene.getMethod("<target.exercise2.LeafClass: void doSomething()>");
-				cg.addNode(entryPoint);
-
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-				
-				method = scene.getMethod("<target.exercise2.OtherLeafClass: void doSomething()>");
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-				
-				method = scene.getMethod("<target.exercise2.Specialization: void doSomething()>");
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-
-				
-				method = scene.getMethod("<target.exercise2.Subclass: void doSomething()>");
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-				
-/*
-				 // things that aren't instantiated
-        		Assert.assertFalse(callsFromMain.contains(thirdLeafMethod));
-        		Assert.assertFalse(callsFromMain.contains(fourthLeafMethod));
-*/
-//				method = scene.getMethod("<target.exercise2.ThirdLeafClass: void doSomething()>");
-//				cg.addNode(method);
-//				cg.addEdge(entryPoint, method);
-//				
-//				method = scene.getMethod("<target.exercise2.FourthLeafClass: void doSomething()>");
-//				cg.addNode(method);
-//				cg.addEdge(entryPoint, method);
-				
+			if (entryPoint.toString().equals("<target.exercise2.Starter: void main(java.lang.String[])>")) {
+				checkMethod(null, entryPoint, cg, scene);
 			}
 		});
 
-    }
+	}
+
+    private void checkMethod(SootMethod src, SootMethod entryPoint, CallGraph cg, Scene scene) {
+		// TODO Auto-generated method stub
+		try {
+
+			if (!cg.hasNode(entryPoint)) {
+				cg.addNode(entryPoint);
+				// System.out.println("added node: " + entryPoint);
+			}
+
+			if (src != null && !cg.hasEdge(src, entryPoint)) {
+				cg.addEdge(src, entryPoint);
+				 System.out.println(src + " -> " + entryPoint);
+			}
+
+			if (entryPoint.hasActiveBody()) {
+				Body b = entryPoint.getActiveBody();
+				for (Unit u : b.getUnits()) {
+
+					Stmt s = (Stmt) u;
+					if (s.containsInvokeExpr()) {
+
+						InvokeExpr expr = s.getInvokeExpr();
+						checkMethod(entryPoint, expr.getMethod(), cg, scene);
+					}
+
+				}
+
+			} else if (entryPoint.isAbstract()) {
+				scene.getApplicationClasses().forEach(sampleClass -> {
+					sampleClass.getInterfaces().forEach(implementedInterface -> {
+						if (implementedInterface == entryPoint.getDeclaringClass()) {
+							sampleClass.getMethods().forEach(method -> {
+								if (method.getSubSignature().equals(entryPoint.getSubSignature())) {
+									if (!cg.hasNode(method)) {
+										cg.addNode(method);
+									}
+
+									if (!cg.hasEdge(src, method)) {
+										cg.addEdge(src, method);
+									}
+
+									checkMethod(entryPoint, method, cg, scene);
+								}
+							});
+						}
+					});
+				});
+
+			}
+
+		} catch (Exception e) {
+			System.out.println("EXCEPTION:");
+		} finally {
+		}
+	}
 
 }
