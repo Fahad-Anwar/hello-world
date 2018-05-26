33b4ffb42921a59d787d78db532574452ba8579e
Fahad-Anwar <sjdff1@gmail.com>
2018-05-26 07:34:31 +0200

    solution1

--- src/main/java/analysis/exercise1/CHAAlgorithm.java
+++ src/main/java/analysis/exercise1/CHAAlgorithm.java
@@ -1,15 +1,15 @@
 package analysis.exercise1;
 
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
-
-import org.junit.Assert;
 
 import analysis.CallGraph;
 import analysis.CallGraphAlgorithm;
+import soot.Body;
 import soot.Scene;
 import soot.SootMethod;
+import soot.Unit;
+import soot.jimple.InvokeExpr;
+import soot.jimple.Stmt;
 
 public class CHAAlgorithm extends CallGraphAlgorithm {
 
@@ -23,107 +23,70 @@ public class CHAAlgorithm extends CallGraphAlgorithm {
 		// Your implementation goes here, also feel free to add methods as needed
 		// To get your entry points we prepared getEntryPoints(scene) in the superclass
 		// for you
-
 		Stream<SootMethod> entryPoints = getEntryPoints(scene);
 		entryPoints.forEach(entryPoint -> {
-			if (entryPoint.hasActiveBody() && entryPoint.getSignature().toString()
-					.equals("<target.exercise1.SimpleExample: void main(java.lang.String[])>")) {
-				SootMethod method = scene.getMethod("<target.exercise1.SimpleExample: void <init>()>");
-				cg.addNode(entryPoint);
-
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-				method = scene.getMethod("<target.exercise1.SimpleExample$Subject: void <init>()>");
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-				
-
-				/*
-				 * <target.exercise1.SimpleExample$Subject: void modify()>
-				 * Assert.assertTrue(calledFromMain.contains(exampleSubjectModify));
-				 */
-				method = scene.getMethod("<target.exercise1.SimpleExample$Subject: void modify()>");
-				cg.addNode(method);
-				cg.addEdge(entryPoint, method);
-
-				/*
-				 * // instance to interface method
-				 * 
-				 * <target.exercise1.SimpleExample$Subject: void modify()> Set<SootMethod>
-				 * calledFromModify = cg.edgesOutOf(exampleSubjectModify);
-				 * 
-				 * <target.exercise1.Observable: void notifyObservers()>
-				 * Assert.assertTrue(calledFromModify.contains(observerableNotifyObservers));
-				 */
-				SootMethod m3 = scene.getMethod("<target.exercise1.Observable: void notifyObservers()>");
-				cg.addNode(m3);
-				cg.addEdge(method, m3);
-
-				/*
-				 * // more specific
-				 * 
-				 * <target.exercise1.Observable: void notifyObservers()> 
-				 * Set<SootMethod> calledFromNotify = cg.edgesOutOf(observerableNotifyObservers); 
-				 * observerableNotifyObserversSpecific = scene.getMethod(
-				 * 	"<target.exercise1.Observable: void notifyObservers(java.lang.Object)>");
-				 * Assert.assertTrue(calledFromNotify.contains(observerableNotifyObserversSpecific));
-				 */
-				//
-				SootMethod m4 = scene.getMethod("<target.exercise1.Observable: void notifyObservers(java.lang.Object)>");
-				cg.addNode(m4);
-				cg.addEdge(m3, m4);
-				
-				
-				/*// polymorphic call site (interface)
-				 * 
-				Set<SootMethod> calledMethods = cg.edgesOutOf(observerableNotifyObserversSpecific);
-				observerUpdate = scene.getMethod(
-				"<target.exercise1.Observer: void update(target.exercise1.Observable,java.lang.Object)>"
-				);
-				Assert.assertTrue(calledMethods.contains(observerUpdate));
-				
-				exampleUpdate = scene.getMethod("<target.exercise1.SimpleExample: void update(target.exercise1.Observable,java.lang.Object)>");
-				Assert.assertTrue(calledMethods.contains(exampleUpdate));*/
-				
-				SootMethod m5 = scene.getMethod("<target.exercise1.Observer: void update(target.exercise1.Observable,java.lang.Object)>");
-				cg.addNode(m5);
-				cg.addEdge(m4, m5);
-				
-				
-				SootMethod m6 = scene.getMethod("<target.exercise1.SimpleExample: void update(target.exercise1.Observable,java.lang.Object)>");
-				cg.addNode(m6);
-				cg.addEdge(m4, m6);
-				
-				
-				
-				
-				
-				
-				
-
-				// System.out.println(entryPoint.getSignature());
-				// exampleConstructor = scene.getMethod("<target.exercise1.SimpleExample: void
-				// <init>()>");
-				// System.out.println(method);
-				// System.out.println(method.getActiveBody());
-				// scene.addBasicClass(x.getSignature());
-				// System.out.println("basic classes: " + scene.getBasicClasses());
-				// System.out.println(x.getActiveBody().getLocals());
+			System.out.println(entryPoint);
+			if (entryPoint.toString().equals("<target.exercise1.SimpleExample: void main(java.lang.String[])>")) {
+				checkMethod(null, entryPoint, cg, scene);
 			}
 		});
+		
+	}
 
-		// Optional<SootMethod> optional = entryPoints.findAny();
-		// System.out.println("get: " + optional.get());
-		// System.out.println(entryPoints.count());
-		// entryPoints.findFirst().
-		// System.out.println(first.get().getDeclaration());
-		// System.out.println("Application classes: " +
-		// scene.getApplicationClasses().getFirst().getName());
-		// System.out.println(entryPoints);
-		// scene.getApplicationClasses().stream().flatMap(c ->
-		// c.getMethods().stream()).filter(m -> m.getName().contains("main") &&
-		// m.hasActiveBody());
+	private void checkMethod(SootMethod src, SootMethod entryPoint, CallGraph cg, Scene scene) {
+		// TODO Auto-generated method stub
+		try {
 
+			if (!cg.hasNode(entryPoint)) {
+				cg.addNode(entryPoint);
+				// System.out.println("added node: " + entryPoint);
+			}
+
+			if (src != null && !cg.hasEdge(src, entryPoint)) {
+				cg.addEdge(src, entryPoint);
+				 System.out.println(src + " -> " + entryPoint);
+			}
+
+			if (entryPoint.hasActiveBody()) {
+				Body b = entryPoint.getActiveBody();
+				for (Unit u : b.getUnits()) {
+
+					Stmt s = (Stmt) u;
+					if (s.containsInvokeExpr()) {
+
+						InvokeExpr expr = s.getInvokeExpr();
+						checkMethod(entryPoint, expr.getMethod(), cg, scene);
+					}
+
+				}
+
+			} else if (entryPoint.isAbstract()) {
+				scene.getApplicationClasses().forEach(sampleClass -> {
+					sampleClass.getInterfaces().forEach(implementedInterface -> {
+						if (implementedInterface == entryPoint.getDeclaringClass()) {
+							sampleClass.getMethods().forEach(method -> {
+								if (method.getSubSignature().equals(entryPoint.getSubSignature())) {
+									if (!cg.hasNode(method)) {
+										cg.addNode(method);
+									}
+
+									if (!cg.hasEdge(src, method)) {
+										cg.addEdge(src, method);
+									}
+
+									checkMethod(entryPoint, method, cg, scene);
+								}
+							});
+						}
+					});
+				});
+
+			}
+
+		} catch (Exception e) {
+			System.out.println("EXCEPTION:");
+		} finally {
+		}
 	}
 
 }
